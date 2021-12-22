ba03574a95abd06560d512fade35e9d83f27c7e5
Harold Gutierrez Calderon <harold.gutierrez@nutanix.com>
2021-12-22 23:46:13 +0100

    Introduction of VmReporter and other minor changes

--- sre_perf.py
+++ sre_perf.py
@@ -46,6 +46,7 @@ class NodeReporter(Reporter):
     self.FIELD_NAMES=["node_name", "hypervisor_cpu_usage_ppm","hypervisor_memory_usage_ppm",
                   "num_iops", "avg_io_latency_usecs", "io_bandwidth_kBps"]
     self.ARITHMOS_ENTITY_PROTO = ArithmosEntityProto.kNode
+    self.max_node_name_width = 0
 
   def overal_live_report(self):
     all_nodes = []
@@ -58,33 +59,98 @@ class NodeReporter(Reporter):
         "num_iops": node_stat.stats.common_stats.num_iops,
         "avg_io_latency_msecs": node_stat.stats.common_stats.avg_io_latency_usecs / 1000,
         "io_bandwidth_kBps": float(node_stat.stats.common_stats.io_bandwidth_kBps /1024)
-      }	
-      all_nodes.append(node)	
+      }
+
+      # Set value to slice node name. Max is 30 character. This is used for displaying the information
+      if len(node["node_name"]) > 30:
+        self.max_node_name_width = 30
+        node["node_name"] = node["node_name"][:30]            
+      elif len(node["node_name"]) > self.max_node_name_width:
+        self.max_node_name_width = len(node["node_name"])
+      all_nodes.append(node)
     return all_nodes
 
+
+class VmReporter(Reporter):
+  """Reports for UVMs"""
+
+  def __init__(self):
+    Reporter.__init__(self)
+    self.FIELD_NAMES=["vm_name", "hypervisor_cpu_usage_ppm"]
+    self.ARITHMOS_ENTITY_PROTO = ArithmosEntityProto.kVM
+    self.max_vm_name_width = 0
+
+  def overal_live_report(self):
+    all_vms = []
+    stats = self.get_stats()
+#    print(stats)
+    for vm_stat in stats:
+      vm = {
+	"vm_name": vm_stat.vm_name,
+        "hypervisor_cpu_usage_ppm": vm_stat.stats.hypervisor_cpu_usage_ppm / 10000
+      }
+
+      # Set value to slice vm name. Max is 30 character. This is used for displaying the information
+      if len(vm["vm_name"]) > 30:
+        self.max_vm_name_width = 30
+        vm["vm_name"] = vm["vm_name"][:30]            
+      elif len(vm["vm_name"]) > self.max_vm_name_width:
+        self.max_vm_name_width = len(vm["vm_name"])
+      
+      all_vms.append(vm)	
+    return all_vms
+  
   
 class Ui(object):
   """Display base"""
 
   def __init__(self):
     self.node_reporter = NodeReporter()
-
+    self.vm_reporter = VmReporter()
     
 class UiCli(Ui):
   """CLI interface"""
 
-  def print_overal_live_report(self, sec, count):
+  def nodes_overal_live_report(self, sec, count):
     i = 0
     while i < count:
       i += 1
       today = datetime.date.today()
       time_now = datetime.datetime.now().strftime("%H:%M:%S")
-      print("{0:<13} {1:<10} {2:>6} {3:>6} {4:>6} {5:>6} {6:>6}".format(str(today), "Node","CPU%","MEM%", "IOPs", "B/W", "LAT"))
-      for node in self.node_reporter.overal_live_report():
-        print("{0:<13} {n[node_name]:<10} {n[hypervisor_cpu_usage_percent]:>6.2f} {n[hypervisor_memory_usage_percent]:>6.2f} {n[num_iops]:>6} {n[io_bandwidth_kBps]:>6.2f} {n[avg_io_latency_msecs]:>6.2f}".format(str(time_now), n=node))
+      nodes = self.node_reporter.overal_live_report()      
+      print("{time:<11} {node:<{width}} {cpu:>6} {mem:>6} {iops:>6} {bw:>6} {lat:>6}".format(
+        time=str(today),
+        node="Node",
+        cpu="CPU%",
+        mem="MEM%",
+        iops="IOPs",
+        bw = "B/W",
+        lat="LAT",
+        width=self.node_reporter.max_node_name_width
+      ))
+      for node in nodes:
+        print("{time:<11} {n[node_name]:<{width}} {n[hypervisor_cpu_usage_percent]:>6.2f} {n[hypervisor_memory_usage_percent]:>6.2f} {n[num_iops]:>6} {n[io_bandwidth_kBps]:>6.2f} {n[avg_io_latency_msecs]:>6.2f}".format(time=str(time_now), n=node, width=self.node_reporter.max_node_name_width))
       print("")
       time.sleep(sec)
 
+  def uvms_overal_live_report(self, sec, count):
+    i = 0
+    while i < count:
+      i += 1
+      today = datetime.date.today()
+      time_now = datetime.datetime.now().strftime("%H:%M:%S")
+      vms = self.vm_reporter.overal_live_report()
+      print("{time:<11} {vm:<{width}} {cpu:>6}".format(
+        time=str(today),
+        vm="VM Name",
+        cpu="CPU%",
+        width=self.vm_reporter.max_vm_name_width
+      ))
+      for vm in vms:
+        print("{0:<11} {v[vm_name]:<{width}} {v[hypervisor_cpu_usage_ppm]:>6.2f}".format(str(time_now), v=vm, width=self.vm_reporter.max_vm_name_width))
+      print("")
+      time.sleep(sec)
+      
       
 class UiInteractive(Ui):
   """Interactive interface"""
@@ -171,6 +237,7 @@ if __name__ == "__main__":
     epilog='"When you eliminate the impossible, whatever remains, however improbable, must be the truth." Spock.'
   )
   parser.add_argument('--nodes', '-n', action='store_true', help="Overal nodes activity report")
+  parser.add_argument('--uvms', '-v', action='store_true', help="Overal user VMs activity report")  
   parser.add_argument('sec', type=int, nargs="?", default=3, help="Interval in seconds")
   parser.add_argument('count', type=int, nargs="?", default=1000, help="Number of iterations")
   args = parser.parse_args()
@@ -179,11 +246,17 @@ if __name__ == "__main__":
   if args.nodes:
     ui_cli = UiCli()
     try:
-      ui_cli.print_overal_live_report(args.sec, args.count)
+      ui_cli.nodes_overal_live_report(args.sec, args.count)
     except KeyboardInterrupt:
       print("Goodbye")
       exit(0)
-      
+  elif args.uvms:
+    ui_cli = UiCli()
+    try:
+      ui_cli.uvms_overal_live_report(args.sec, args.count)
+    except KeyboardInterrupt:
+      print("Goodbye")
+      exit(0)    
   else:
     ui_interactive = UiInteractive()
     curses.wrapper(ui_interactive.open_screen)
