65fe789afe4803a7740d5c5fefdcd980ba8e1eaf
Harold Gutierrez Calderon <harold.gutierrez@nutanix.com>
2021-12-25 19:40:24 +0100

    + Replaces stats_util.get_master_arithmos_entities with serviceability.interface.analytics.arithmos_rpc_client for querying Arithmos stats. + Complete first version of uvms_overal_live_report. + Format several segments of the code to make it more PEP compliance.

--- sre_perf.py
+++ sre_perf.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2021 Nutanix Inc. All rights reserved.
-#
 # Author: harold.gutierrez@nutanix.com
 #
 # Report cluster activity from arithmos
@@ -22,20 +20,35 @@ import curses
 
 from stats.arithmos import stats_util
 from util.interfaces.interfaces import NutanixInterfaces
-from stats.arithmos.interface.arithmos_type_pb2 import ArithmosEntityProto, VM
+from stats.arithmos.interface.arithmos_type_pb2 import *
+from serviceability.interface.analytics.arithmos_rpc_client import ArithmosDataProcessing
+from stats.arithmos.interface.arithmos_interface_pb2 import (
+    AgentGetEntitiesArg, MasterGetEntitiesArg)
 
 class Reporter(object):
   """Reporter base """
 
   def __init__(self):
     self.interfaces = NutanixInterfaces()
+    self.arithmos_interface = ArithmosDataProcessing()
     self.arithmos_client = self.interfaces.arithmos_client
     self.FIELD_NAMES=[]
     
-  def get_stats(self):
-    return stats_util.get_master_arithmos_entities(
-      self.interfaces.arithmos_client, self.ARITHMOS_ENTITY_PROTO,
-      field_names=self.FIELD_NAMES)	
+  def _get_live_stats(self, entity_type, sort_criteria=None, filter_criteria=None,
+                     search_term=None, field_name_list=None):
+    arithmos_interface = ArithmosDataProcessing()
+    ret = arithmos_interface.MasterGetEntitiesStats(
+      entity_type, sort_criteria, filter_criteria, search_term, requested_field_name_list=field_name_list)
+    if ret:
+      response = ret.response
+      if response.error == ArithmosErrorProto.kNoError:
+        return response
+
+  def _get_generic_stats_dict(self, generic_stat_list):
+    ret = {}
+    for generic_stat in generic_stat_list:
+      ret[generic_stat.stat_name] = generic_stat.stat_value
+    return ret
 
   
 class NodeReporter(Reporter):
@@ -43,14 +56,24 @@ class NodeReporter(Reporter):
 
   def __init__(self):
     Reporter.__init__(self)
-    self.FIELD_NAMES=["node_name", "hypervisor_cpu_usage_ppm","hypervisor_memory_usage_ppm",
-                  "num_iops", "avg_io_latency_usecs", "io_bandwidth_kBps"]
-    self.ARITHMOS_ENTITY_PROTO = ArithmosEntityProto.kNode
+    self._ARITHMOS_ENTITY_PROTO = ArithmosEntityProto.kNode    
     self.max_node_name_width = 0
 
+  def _get_node_live_stats(self, sort_criteria=None, filter_criteria=None,
+                           search_term=None, field_name_list=None):
+    response = self._get_live_stats(self._ARITHMOS_ENTITY_PROTO,
+                                    sort_criteria, filter_criteria,
+                                    search_term, field_name_list)
+    entity_list = response.entity_list.node
+    return entity_list
+    
   def overal_live_report(self):
+    field_names=["node_name", "hypervisor_cpu_usage_ppm",
+                 "hypervisor_memory_usage_ppm","num_iops",
+                 "avg_io_latency_usecs", "io_bandwidth_kBps"]
+
     all_nodes = []
-    stats = self.get_stats()
+    stats = self._get_node_live_stats(field_name_list=field_names)
     for node_stat in stats:
       node = {
 	"node_name": node_stat.node_name,
@@ -61,7 +84,8 @@ class NodeReporter(Reporter):
         "io_bandwidth_kBps": float(node_stat.stats.common_stats.io_bandwidth_kBps /1024)
       }
 
-      # Set value to slice node name. Max is 30 character. This is used for displaying the information
+      # Set value to slice node name. Max is 30 character. This is used for
+      # displaying the information
       if len(node["node_name"]) > 30:
         self.max_node_name_width = 30
         node["node_name"] = node["node_name"][:30]            
@@ -76,21 +100,46 @@ class VmReporter(Reporter):
 
   def __init__(self):
     Reporter.__init__(self)
-    self.FIELD_NAMES=["vm_name", "hypervisor_cpu_usage_ppm"]
     self.ARITHMOS_ENTITY_PROTO = ArithmosEntityProto.kVM
     self.max_vm_name_width = 0
 
+  def _get_vm_live_stats(self, sort_criteria=None, filter_criteria=None,
+                           search_term=None, field_name_list=None):
+    response = self._get_live_stats(self.ARITHMOS_ENTITY_PROTO,
+                                    sort_criteria, filter_criteria,
+                                    search_term, field_name_list)
+    entity_list = response.entity_list.vm
+    return entity_list
+
+    
   def overal_live_report(self):
+    field_names=["vm_name", "hypervisor_cpu_usage_ppm", "memory_usage_ppm",
+                 "controller_num_iops", "controller_io_bandwidth_kBps",
+                 "controller_avg_io_latency_usecs"]
     all_vms = []
-    stats = self.get_stats()
-#    print(stats)
+    stats = self._get_vm_live_stats(field_name_list=field_names, sort_criteria="vm_name")
+    #print(stats)
     for vm_stat in stats:
+
+      generic_stats = self._get_generic_stats_dict(vm_stat.stats.generic_stat_list)
+      # For VMs without memory_usage_ppm assign 0 instead
+      if "memory_usage_ppm" not in generic_stats:
+        generic_stats[u"memory_usage_ppm"] = 0
+        
       vm = {
 	"vm_name": vm_stat.vm_name,
-        "hypervisor_cpu_usage_ppm": vm_stat.stats.hypervisor_cpu_usage_ppm / 10000
+        "hypervisor_cpu_usage_percent": vm_stat.stats.hypervisor_cpu_usage_ppm
+        / 10000,
+        "memory_usage_percent": generic_stats["memory_usage_ppm"] /10000,
+        "controller_num_iops": vm_stat.stats.common_stats.controller_num_iops,
+        "controller_io_bandwidth_kBps":
+        vm_stat.stats.common_stats.controller_io_bandwidth_kBps /1024,
+        "controller_avg_io_latency_msecs":
+        vm_stat.stats.common_stats.controller_avg_io_latency_usecs / 1000
       }
 
-      # Set value to slice vm name. Max is 30 character. This is used for displaying the information
+      # Set value to slice vm name. Max is 30 character. This is used for
+      # displaying the information
       if len(vm["vm_name"]) > 30:
         self.max_vm_name_width = 30
         vm["vm_name"] = vm["vm_name"][:30]            
@@ -99,7 +148,7 @@ class VmReporter(Reporter):
       
       all_vms.append(vm)	
     return all_vms
-  
+      
   
 class Ui(object):
   """Display base"""
@@ -117,19 +166,29 @@ class UiCli(Ui):
       i += 1
       today = datetime.date.today()
       time_now = datetime.datetime.now().strftime("%H:%M:%S")
-      nodes = self.node_reporter.overal_live_report()      
-      print("{time:<11} {node:<{width}} {cpu:>6} {mem:>6} {iops:>6} {bw:>6} {lat:>6}".format(
-        time=str(today),
-        node="Node",
-        cpu="CPU%",
-        mem="MEM%",
-        iops="IOPs",
-        bw = "B/W",
-        lat="LAT",
-        width=self.node_reporter.max_node_name_width
+      nodes = self.node_reporter.overal_live_report()
+      print("{time:<11} {node:<{width}} {cpu:>6} {mem:>6} {iops:>6} {bw:>6} "
+            "{lat:>6}".format(
+              time=str(today),
+              node="Node",
+              cpu="CPU%",
+              mem="MEM%",
+              iops="IOPs",
+              bw = "B/W",
+              lat="LAT",
+              width=self.node_reporter.max_node_name_width
       ))
       for node in nodes:
-        print("{time:<11} {n[node_name]:<{width}} {n[hypervisor_cpu_usage_percent]:>6.2f} {n[hypervisor_memory_usage_percent]:>6.2f} {n[num_iops]:>6} {n[io_bandwidth_kBps]:>6.2f} {n[avg_io_latency_msecs]:>6.2f}".format(time=str(time_now), n=node, width=self.node_reporter.max_node_name_width))
+        print("{time:<11} "
+              "{n[node_name]:<{width}} "
+              "{n[hypervisor_cpu_usage_percent]:>6.2f} "
+              "{n[hypervisor_memory_usage_percent]:>6.2f} "
+              "{n[num_iops]:>6} "
+              "{n[io_bandwidth_kBps]:>6.2f} "
+              "{n[avg_io_latency_msecs]:>6.2f} "
+              .format(time=str(time_now),
+                      n=node,
+                      width=self.node_reporter.max_node_name_width))
       print("")
       time.sleep(sec)
 
@@ -140,14 +199,27 @@ class UiCli(Ui):
       today = datetime.date.today()
       time_now = datetime.datetime.now().strftime("%H:%M:%S")
       vms = self.vm_reporter.overal_live_report()
-      print("{time:<11} {vm:<{width}} {cpu:>6}".format(
-        time=str(today),
-        vm="VM Name",
-        cpu="CPU%",
-        width=self.vm_reporter.max_vm_name_width
+      print("{time:<11} {vm:<{width}} {cpu:>6} {mem:>6} {iops:>6} {bw:>6} "
+            "{lat:>6}".format(
+              time=str(today),
+              vm="VM Name",
+              cpu="CPU%",
+              mem="MEM%",
+              iops="IOPs",
+              bw = "B/W",
+              lat="LAT",
+              width=self.vm_reporter.max_vm_name_width
       ))
       for vm in vms:
-        print("{0:<11} {v[vm_name]:<{width}} {v[hypervisor_cpu_usage_ppm]:>6.2f}".format(str(time_now), v=vm, width=self.vm_reporter.max_vm_name_width))
+        print("{0:<11} {v[vm_name]:<{width}} "
+              "{v[hypervisor_cpu_usage_percent]:>6.2f} "
+              "{v[memory_usage_percent]:>6.2f} "
+              "{v[controller_num_iops]:>6} "
+              "{v[controller_io_bandwidth_kBps]:>6.2f} "
+              "{v[controller_avg_io_latency_msecs]:>6.2f} "
+              .format(str(time_now),
+                      v=vm,
+                      width=self.vm_reporter.max_vm_name_width))
       print("")
       time.sleep(sec)
       
@@ -237,7 +309,8 @@ if __name__ == "__main__":
     epilog='"When you eliminate the impossible, whatever remains, however improbable, must be the truth." Spock.'
   )
   parser.add_argument('--nodes', '-n', action='store_true', help="Overal nodes activity report")
-  parser.add_argument('--uvms', '-v', action='store_true', help="Overal user VMs activity report")  
+  parser.add_argument('--uvms', '-v', action='store_true', help="Overal user VMs activity report")
+  parser.add_argument('--test', '-t', action='store_true', help="Place holder for testing new features")    
   parser.add_argument('sec', type=int, nargs="?", default=3, help="Interval in seconds")
   parser.add_argument('count', type=int, nargs="?", default=1000, help="Number of iterations")
   args = parser.parse_args()
@@ -250,13 +323,20 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
       print("Goodbye")
       exit(0)
+      
   elif args.uvms:
     ui_cli = UiCli()
     try:
       ui_cli.uvms_overal_live_report(args.sec, args.count)
     except KeyboardInterrupt:
-      print("Goodbye")
-      exit(0)    
+      print("Goodbye : )")
+      exit(0)
+      
+  elif args.test:
+      print("==== TESTING ====")
+      #vm_reporter = VmReporter()
+      #vm_reporter.test_data_processing()
+      
   else:
     ui_interactive = UiInteractive()
     curses.wrapper(ui_interactive.open_screen)
